a5ff2de775d21ca5f7f2c83a2b47190b34f9b349
PhilippRoessner <philipproessner@hotmail.com>
2017-04-17 16:31:59 +0200

    Add project files.

---
+++ XFormsControls/XFormsControls/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFormsControls
+{
+    public partial class MainPage : ContentPage
+    {
+        private MyGloriousItem _selectedListItem;
+        public MyGloriousItem selectedListItem {
+            get
+            {
+                return _selectedListItem;
+            }
+            set
+            {
+                _selectedListItem = value;
+                OnPropertyChanged();
+            }
+        }
+        private List<MyGloriousItem> _liste;
+        public List<MyGloriousItem> liste { get
+            {
+                return _liste ?? (_liste = neueListeBauen());
+            }
+        }
+    private double _selectedValue = 25;
+    public double selectedValue
+    {
+        get
+        {
+            return _selectedValue;
+        }
+        set
+        {
+            _selectedValue = value;
+            OnPropertyChanged();
+        }
+    }
+        private List<MyGloriousItem> neueListeBauen()
+        {
+            var neueliste = new List<MyGloriousItem>();
+            neueliste.Add(new MyGloriousItem() { id = "Erster", val = 2.01 });
+            neueliste.Add(new MyGloriousItem() { id = "Zweiter", val = 7.21 });
+            neueliste.Add(new MyGloriousItem() { id = "Dritter",val = 1.00});
+            return neueliste;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+    public class MyGloriousItem
+    {
+        public string id { get; set; }
+        public double val { get; set; }
+    }
+}
---
+++ XFormsControls/XFormsControls/Numpad.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XFormsControls {
+    public partial class Numpad : ContentView {
+        private static double defaultVal = 0;
+        public static readonly BindableProperty valueProperty = BindableProperty.Create(
+                                                            propertyName: "Value",
+                                                            returnType: typeof(double),
+                                                            declaringType: typeof(Numpad),
+                                                            defaultValue: defaultVal,
+                                                            defaultBindingMode: BindingMode.TwoWay);
+
+        public double Value {
+            get { return (double)GetValue(valueProperty); }
+            set {
+                SetValue(valueProperty, value);
+            }
+        }
+        //private static void HandleButtonPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        //{
+        //    var button = (ExtendedButton)bindable;
+        //    button.UpdateButtonState();
+        //}
+        //public string Value { get; set; }
+        public void NumButtonClicked(object sender, EventArgs e) {
+            var button = (Button)sender;
+            var neuerString = (Value.ToString("N" + Dezimalstellen) + button.Text);
+            neuerString = MoveDelimitter(neuerString);
+            Value = Double.Parse(neuerString);
+        }
+
+        private string MoveDelimitter(string neuerString) {
+            neuerString = neuerString.Replace(".", "");
+            neuerString = neuerString.Insert(neuerString.Length - Dezimalstellen, ".");
+            return neuerString;
+        }
+
+        public void DelButtonClicked(object sender, EventArgs e) {
+            var button = (Button)sender;
+            var stringval = Value.ToString("N"+Dezimalstellen);
+            if (Value < 0.0) {
+                Value = 0.00;
+            } else {
+                var neuerString = stringval.Substring(0, stringval.Length - 1);
+                neuerString = MoveDelimitter(neuerString);
+                Value = Double.Parse(neuerString);
+            }
+        }
+        public void OkButtonClicked(object sender, EventArgs e) {
+            this.Dezimalstellen = 0;
+            Value = Math.Round(Value, Dezimalstellen);
+            //lblValue.SetBinding(targetProperty: this.Value,"", stringFormat: "{0:0.00}");
+        }
+        public int Dezimalstellen = 2;
+        private double _value { get; set; }
+        //public double Value {
+        //    get {
+        //        return _value;
+        //    }
+        //    set {
+        //        if (_value != value) {
+        //            _value = value;
+        //            OnPropertyChanged();
+        //            OnPropertyChanged("ViewValue");
+        //        }
+        //    }
+        //}
+        public string ViewValue { get {
+                return _value.ToString("N"+Dezimalstellen);
+            }
+        }
+        public Numpad() {
+
+            InitializeComponent();
+            //var entry = new Numpad();
+            //entry.SetBinding(Numpad.valueProperty, "Value");
+            BindingContext = this;
+        }
+    }
+}
